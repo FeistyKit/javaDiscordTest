94bf8396603e4fab06b682dee7e6180c4ef82aba
FeistyKit <eeveebeevee33@gmail.com>
2021-10-13 15:19:59 -0400

    Introduced reading from file into ArrayList<String>

--- src/main/java/com/feisty/App.java
+++ src/main/java/com/feisty/App.java
@@ -1,12 +1,13 @@
 package com.feisty;
 import com.feisty.javaDiscordTest.FileReader;
+import java.util.ArrayList;
 
 public class App
 {
     public static void main( String[] args )
     {
         try {
-        String f = FileReader.readToString("test.txt");
+        ArrayList<String> f = FileReader.readToArrayList("test.txt");
         System.out.println(f);
         } catch (Exception e) {
             e.printStackTrace();
