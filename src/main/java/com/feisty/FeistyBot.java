00e74cb5eba1a6a0583be78e07571090b1468139
FeistyKit <eeveebeevee33@gmail.com>
2021-10-17 21:55:45 -0400

    tried to copy n paste the basic bot tutorial

--- src/main/java/com/feisty/FeistyBot.java
+++ src/main/java/com/feisty/FeistyBot.java
@@ -1,26 +1,54 @@
 package com.feisty.javaDiscordTest;
 
-import com.feisty.javaDiscordTest.FileReader;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
-import reactor.core.publisher.Mono;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.object.entity.User;
 import java.io.FileNotFoundException;
+import reactor.core.publisher.Mono;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 
 public class FeistyBot {
-    public DiscordClient client;
-    public Mono login;
+  public DiscordClient client;
+  public Mono login;
 
-    public FeistyBot(String authFileName, int tokenLine) throws FileNotFoundException {
-        String auth = (String) FileReader.readToArrayList(authFileName).toArray()[tokenLine];
-        this.client = DiscordClient.create(auth);
-        login = client.withGateway((GatewayDiscordClient gateway) ->
-            gateway.on(ReadyEvent.class, event ->
-                        Mono.fromRunnable(() -> {
-                                final User self = event.getSelf();
-                                System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
-                        }
-        )));
-    }
+  public FeistyBot(String authFileName, int tokenLine) throws FileNotFoundException {
+    String auth = (String) FileReader.readToArrayList(authFileName).toArray()[tokenLine];
+    this.client = DiscordClient.create(auth);
+    login =
+        client.withGateway(
+            (GatewayDiscordClient gateway) -> {
+              Mono<Void> printOnLogin =
+                  gateway
+                      .on(
+                          ReadyEvent.class,
+                          event ->
+                              Mono.fromRunnable(
+                                  () -> {
+                                    final User self = event.getSelf();
+                                    System.out.printf(
+                                        "Logged in as %s#%s%n",
+                                        self.getUsername(), self.getDiscriminator());
+                                  }))
+                      .then();
+              // MessageCreateEvent example
+              Mono<Void> handlePingCommand =
+                  gateway
+                      .on(
+                          MessageCreateEvent.class,
+                          event -> {
+                            Message message = event.getMessage();
+                            if (message.getContent().equalsIgnoreCase("!ping")) {
+                              return message
+                                  .getChannel()
+                                  .flatMap(channel -> channel.createMessage("pong!"));
+                            }
+                            return Mono.empty();
+                          })
+                      .then();
+              // combine them!
+              return printOnLogin.and(handlePingCommand);
+            });
+  }
 }
